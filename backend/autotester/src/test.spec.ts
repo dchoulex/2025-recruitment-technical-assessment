3b2d8fdfcb668a78f6ba0262e84c73fa9f66d213
David Choulex <davidchoulex@Davids-MacBook-Air-3.local>
2025-02-14 09:34:09 +0900

    Finish assessment

--- backend/autotester/src/test.spec.ts
+++ backend/autotester/src/test.spec.ts
@@ -18,8 +18,33 @@ describe("Task 1", () => {
       expect(response.body).toStrictEqual({ msg: "Alpha Alfredo" });
     });
 
+    // Additional test case. Trailing with special characters.
+    it("trailing whitespace", async () => {
+      const response = await getTask1("kAisenDon-----");
+      expect(response.body).toStrictEqual({ msg: "Kaisendon" });
+    });
+
+    // Additional test case. The word starts with special character
+    it("leading whitespace", async () => {
+      const response = await getTask1("----yummy-wagyu");
+      expect(response.body).toStrictEqual({ msg: "Yummy Wagyu" });
+    });
+
     it("error case", async () => {
       const response = await getTask1("");
+      expect(response.status).toBe(400);
+    });
+
+    // Additional test case. When all characters are non letters. 
+    it("error case 2", async () => {
+      const response = await getTask1("@@@1122334");
+      expect(response.status).toBe(400);
+    });
+
+    // Additional test case. All letters are whitespaces. 
+    it("error case 3", async () => {
+      const response = await getTask1("     ");
+      console.log(response.error)
       expect(response.status).toBe(400);
     });
   });
@@ -51,6 +76,17 @@ describe("Task 2", () => {
       };
       const resp1 = await putTask2(meatball);
       expect(resp1.status).toBe(200);
+    });
+
+    // Additional test case. Add recipe with empty list of required items, assume as non vaild data.
+    it("Add Recipe with no required items.", async () => {
+      const friedRice = {
+        type: "recipe",
+        name: "Fried rice",
+        requiredItems: [],
+      };
+      const resp1 = await putTask2(friedRice);
+      expect(resp1.status).toBe(400);
     });
 
     it("Congratulations u burnt the pan pt2", async () => {
@@ -93,6 +129,70 @@ describe("Task 2", () => {
       });
       expect(resp3.status).toBe(400);
     });
+
+    // Additional test case. Check duplicate required items.
+    it("Duplicate required elements.", async () => {
+      const resp = await putTask2({
+        type: "recipe",
+        name: "Yakiniku",
+        requiredItems: [
+          { name: "Beef", quantity: 1 },
+          { name: "Beef", quantity: 1 },
+        ],
+      });
+      expect(resp.status).toBe(400);
+    });
+
+    // Additional test case. Recipe data entry in wrong format.
+    it("Wrong recipe data format.", async () => {
+      const resp = await putTask2({
+        type: "recipe",
+        name: "Tempura",
+        cookTime: 10,
+      });
+
+      expect(resp.status).toBe(400);
+    });
+
+    // Additional test case. Ingredient data entry in wrong format.
+    it("Wrong ingredient data format.", async () => {
+      const resp = await putTask2({
+        type: "ingredient",
+        name: "Taiyaki",
+        requiredItems: [
+          { name: 'Azuki', quantity: 1}
+        ],
+      });
+
+      expect(resp.status).toBe(400);
+    });
+
+    // Additional test case. Recipe data entry requiredItems field has non valid quantity.
+    it("Wrong quantity format in the required items part 1.", async () => {
+      const resp = await putTask2({
+        type: "recipe",
+        name: "Daifuku",
+        requiredItems: [
+          { name: 'Mochi', quantity: -10}
+        ],
+      });
+
+      expect(resp.status).toBe(400);
+    });
+
+    // Additional test case. Recipe data entry requiredItems field has non valid quantity.
+    it("Wrong quantity format in the required items part 2.", async () => {
+      const resp = await putTask2({
+        type: "recipe",
+        name: "Daifuku",
+        requiredItems: [
+          { name: 'Kinoko', quantity: 0}
+        ],
+      });
+
+      expect(resp.status).toBe(400);
+    });
+
   });
 });
 
@@ -155,7 +255,157 @@ describe("Task 3", () => {
       expect(resp2.status).toBe(200);
 
       const resp3 = await getTask3("Skibidi");
+      // console.log(resp3.error);
       expect(resp3.status).toBe(200);
+    });
+
+    // Additional test case. There is a loop in the recipe.
+    it("Recipe is in loop.", async () => {
+      const aburaSoba = {
+        type: "recipe",
+        name: "Abura Soba",
+        requiredItems: [{ name: "Soba", quantity: 1 }],
+      };
+      const resp1 = await postEntry(aburaSoba);
+      expect(resp1.status).toBe(200);
+
+      const soba = {
+        type: "recipe",
+        name: "Soba",
+        requiredItems: [{ name: "Abura Soba", quantity: 2 }],
+      };
+      const resp2 = await postEntry(soba);
+      expect(resp2.status).toBe(200);
+
+      const resp3 = await getTask3("Abura Soba");
+      // console.log(resp3.error);
+      expect(resp3.status).toBe(400);
+
+      const resp4 = await getTask3("Soba");
+      // console.log(resp4.error);
+      expect(resp4.status).toBe(400);
+    });
+
+    // Additional test case. There is a loop in the recipe.
+    // it("Bro is a pro now.", async () => {
+    //   const spaghetti = {
+    //     type: "recipe",
+    //     name: "Skibidi Spaghetti",
+    //     requiredItems: [
+    //       { name: "Oishi Meatball", quantity: 3 },
+    //       { name: "Pasta", quantity: 1 },
+    //       { name: "Tomato", quantity: 2 },
+    //     ],
+    //   };
+    //   const resp1 = await postEntry(spaghetti);
+    //   expect(resp1.status).toBe(200);
+
+    //   const meatball = {
+    //     type: "recipe",
+    //     name: "Oishi Meatball",
+    //     requiredItems: [
+    //       { name: "Beef", quantity: 2 },
+    //       { name: "Pasta", quantity: 1 },
+    //       { name: "Egg", quantity: 1 },
+    //     ],
+    //   };
+    //   const resp2 = await postEntry(meatball);
+    //   // console.log(resp2.error);
+    //   expect(resp2.status).toBe(200);
+
+    //   const pasta = {
+    //     type: "recipe",
+    //     name: "Pasta",
+    //     requiredItems: [
+    //       { name: "Flour", quantity: 3 },
+    //       { name: "Egg", quantity: 1 },
+    //     ],
+    //   };
+    //   const resp3 = await postEntry(pasta);
+    //   expect(resp3.status).toBe(200);
+
+    //   const entries = [
+    //     { type: "ingredient", name: "Beef", cookTime: 5 },
+    //     { type: "ingredient", name: "Egg", cookTime: 3 },
+    //     { type: "ingredient", name: "Flour", cookTime: 0 },
+    //     { type: "ingredient", name: "Tomato", cookTime: 2 },
+    //   ];
+
+    //   for (const entry of entries) {
+    //     const resp4 = await postEntry(entry);
+    //     expect(resp4.status).toBe(200);
+    //   }
+
+    //   const resp5 = await getTask3("Skibidi Spaghetti");
+    //   // console.log(resp5.body);
+    //   expect(resp5.status).toBe(200);
+    // });
+
+    it("Bro is on fire!", async () => {
+      const dish1 = {
+        type: "recipe",
+        name: "Dish1",
+        requiredItems: [
+          { name: "Dish2", quantity: 3 },
+          { name: "Dish3", quantity: 2 },
+          { name: "Dish4", quantity: 2 },
+          { name: "Ing1", quantity: 1 },
+        ],
+      };
+      const resp1 = await postEntry(dish1);
+      expect(resp1.status).toBe(200);
+
+      const dish2 = {
+        type: "recipe",
+        name: "Dish2",
+        requiredItems: [
+          { name: "Dish3", quantity: 3 },
+          { name: "Dish4", quantity: 2 },
+          { name: "Ing2", quantity: 2 },
+        ],
+      };
+
+      const resp2 = await postEntry(dish2);
+      expect(resp2.status).toBe(200);
+
+      const dish3 = {
+        type: "recipe",
+        name: "Dish3",
+        requiredItems: [
+          { name: "Dish4", quantity: 1 },
+          { name: "Ing3", quantity: 2 },
+        ],
+      };
+
+      const resp3 = await postEntry(dish3);
+      expect(resp3.status).toBe(200);
+
+      const dish4 = {
+        type: "recipe",
+        name: "Dish4",
+        requiredItems: [
+          { name: "Ing1", quantity: 2 },
+          { name: "Ing2", quantity: 2 },
+        ],
+      };
+
+      const resp4 = await postEntry(dish4);
+      expect(resp4.status).toBe(200);
+
+      const entries = [
+        { type: "ingredient", name: "Ing1", cookTime: 5 },
+        { type: "ingredient", name: "Ing2", cookTime: 3 },
+        { type: "ingredient", name: "Ing3", cookTime: 0 },
+      ];
+
+      for (const entry of entries) {
+        const resp5 = await postEntry(entry);
+        expect(resp5.status).toBe(200);
+      }
+
+      const resp6 = await getTask3("Dish1");
+      console.log(resp6.body);
+      expect(resp6.status).toBe(200);
     });
   });
 });
--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -1,7 +1,12 @@
 from dataclasses import dataclass
-from typing import List, Dict, Union
+from typing import List, Dict, Union, Set
 from flask import Flask, request, jsonify
 import re
+import sys
+
+# Max number of entries in cookbook is below 10_000. Default python recursion limit is 1_000. 
+# Change the recursion limit to 10_000.
+sys.setrecursionlimit(10 ** 4)
 
 # ==== Type Definitions, feel free to add or modify ===========================
 @dataclass
@@ -16,11 +21,22 @@ class RequiredItem():
 @dataclass
 class Recipe(CookbookEntry):
 	required_items: List[RequiredItem]
+	items: Set[str]
 
 @dataclass
 class Ingredient(CookbookEntry):
 	cook_time: int
 
+@dataclass
+class IngredientSummary():
+	name: str
+	quantity: int
+
+@dataclass
+class RecipeSummary():
+	name: str
+	cook_time: int
+	ingredients: Dict[str, IngredientSummary]
 
 # =============================================================================
 # ==== HTTP Endpoint Stubs ====================================================
@@ -28,7 +44,8 @@ class Ingredient(CookbookEntry):
 app = Flask(__name__)
 
 # Store your recipes here!
-cookbook = None
+# Initiate cookbook as hash map to identify unique name efficiently.
+cookbook = {}
 
 # Task 1 helper (don't touch)
 @app.route("/parse", methods=['POST'])
@@ -44,23 +61,174 @@ def parse():
 # Takes in a recipeName and returns it in a form that 
 def parse_handwriting(recipeName: str) -> Union[str | None]:
 	# TODO: implement me
-	return recipeName
+	if len(recipeName) == 0:
+		return None
+	
+	parsed_name = recipeName
 
+	# To improve readability, divide the parsing logic step by step.
+	# Remove all '-' and '_'.
+	parsed_name = parsed_name.replace('-', ' ').replace('_', ' ')
+
+	# Remove all non letter characters.
+	parsed_name = re.sub(r'[^a-zA-Z ]', '', parsed_name)
+
+	# Capitalize the first letter of each word.
+	parsed_name = parsed_name.lower().title()
+
+	# Remove leading and trailing whitespaces.
+	parsed_name = parsed_name.strip()
+
+	# Squash down multiple whitespaces to a singular whitespace.
+	parsed_name = re.sub(r' {2,}', ' ', parsed_name)
+
+	# Return recipe name if length is > 0 and None otherwise
+	return parsed_name if len(parsed_name) > 0 else None
 
 # [TASK 2] ====================================================================
 # Endpoint that adds a CookbookEntry to your magical cookbook
 @app.route('/entry', methods=['POST'])
 def create_entry():
 	# TODO: implement me
-	return 'not implemented', 500
+	name = request.json.get('name', None)
+	entry_type = request.json.get('type', None)
 
+	if name is None:
+		return 'Invalid name.', 400
+	
+	# Return 400 when entry name already exist. Assume the entry name can only belong to either 'ingredient' or 'recipe' type, not both.
+	if name in cookbook:
+		return 'Data entry already exist.', 400
+	
+	# Return 400 when the entry type is not recipe or ingredient.
+	if entry_type is None or entry_type not in {'recipe', 'ingredient'}:
+		return 'Invalid type.', 400
+	
+	if entry_type == 'recipe':
+		required_items = request.json.get('requiredItems', None)
+
+		# Optional edge case. if the recipe doesn't have required items.
+		if required_items is None or len(required_items) == 0:
+			return 'Invalid required items.', 400
+		
+		recipe = Recipe(name, [], set())
+		
+		for item in required_items:
+			item_name, quantity = item.values()
+
+			# Assume quantity below or equal to 0 is invalid.
+			if int(quantity) <= 0:
+				return 'Quantity is not valid', 400
+
+			# Return 400 when duplicate item exist.
+			if item_name in recipe.items:
+				return 'Duplicate required item found.', 400
+			
+			recipe.items.add(item_name)
+			required_item = RequiredItem(item_name, quantity)
+
+			recipe.required_items.append(required_item)
+
+		cookbook[name] = recipe
+		
+	elif entry_type == 'ingredient':
+		cook_time = request.json.get('cookTime', None)
+
+		# Return 400 when cook time is smaller than 0
+		if cook_time is None or int(cook_time) < 0:
+			return 'Invalid cook time.', 400
+		
+		cookbook[name] = Ingredient(name, cook_time)
+
+	return ' ', 200
 
 # [TASK 3] ====================================================================
 # Endpoint that returns a summary of a recipe that corresponds to a query name
+
+def get_recipe_summary(item_name, cache, visited):
+	if item_name not in cookbook:
+		return None
+
+	if item_name in cache:
+		print('Hit cache!')
+		return cache[item_name]
+	
+	item = cookbook[item_name]
+	
+	if type(item) == Ingredient:
+		return item
+	
+	# Detect cycle
+	if item.name in visited:
+		return None
+	
+	visited.add(item.name)
+
+	required_items = item.required_items
+
+	# Optional edge case. if the recipe doesn't have required items.
+	if required_items is None or len(required_items) == 0:
+		return None
+	
+	recipe_summary = RecipeSummary(item.name, 0, {})
+	
+	for required_item in required_items:
+		summary = get_recipe_summary(required_item.name, cache, visited)
+
+		if summary is None:
+			return None
+		
+		# Summary is an instance of Ingredient
+		if type(summary) == Ingredient:
+			ingredient_name = summary.name
+
+			if ingredient_name in recipe_summary.ingredients:
+				existing_ingredient = recipe_summary.ingredients[ingredient_name]
+				recipe_summary.ingredients[ingredient_name] = IngredientSummary(ingredient_name, existing_ingredient.quantity + required_item.quantity)
+			else:
+				recipe_summary.ingredients[ingredient_name] = IngredientSummary(ingredient_name, required_item.quantity)
+				
+		# Summary is an instance of RecipeSummary
+		else:
+			for ingredient_name in summary.ingredients:
+				additional_ingredient = summary.ingredients[ingredient_name]
+
+				if ingredient_name in recipe_summary.ingredients:
+					existing_ingredient = recipe_summary.ingredients[ingredient_name]
+					recipe_summary.ingredients[ingredient_name] = IngredientSummary(ingredient_name, existing_ingredient.quantity + (additional_ingredient.quantity * required_item.quantity))
+				else:
+					recipe_summary.ingredients[ingredient_name] = IngredientSummary(ingredient_name, additional_ingredient.quantity * required_item.quantity)
+
+		recipe_summary.cook_time += summary.cook_time * required_item.quantity
+
+		cache[item.name] = recipe_summary		
+
+	return recipe_summary
+
 @app.route('/summary', methods=['GET'])
 def summary():
 	# TODO: implement me
-	return 'not implemented', 500
+	# Assume the recipe name always valid
+	recipe_name = request.args.get('name', None)
+
+	# Return 400 if the recipe name cannot be found and the type is ingredient.
+	if recipe_name is None or recipe_name not in cookbook or type(cookbook[recipe_name]) != Recipe:
+		return 'Invalid recipe name', 400
+	
+	cache = {}
+	visited = set()
+	recipe_summary = get_recipe_summary(recipe_name, cache, visited)
+
+	if recipe_summary is None:
+		return 'Recipe contains ingredients or recipes that does not exist or recipe is in loop.', 400
+
+	response_body = {
+		'name': recipe_summary.name,
+		'cookTime': recipe_summary.cook_time,
+		'ingredients': [{'name': ingredient_summary.name, 'quantity': ingredient_summary.quantity} for ingredient_summary in recipe_summary.ingredients.values()]
+	}
+
+	return response_body, 200
 
 
 # =============================================================================
